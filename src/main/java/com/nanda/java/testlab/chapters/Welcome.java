d04880613195ce90cf90faf1f891882893f0de5d
nandakumar <nandakumar.rangasamy@gmail.com>
2017-07-09 12:23:50 +0530

    added new test lab project

---
+++ src/main/java/com/nanda/java/testlab/chapters/Welcome.java
@@ -0,0 +1,11 @@
+package com.nanda.java.testlab.chapters;
+
+public class Welcome {
+	
+	public static void main(String args[]){
+		
+		System.out.println("Welcome to Nanda's Java Test Lab");
+		
+	}
+
+}
